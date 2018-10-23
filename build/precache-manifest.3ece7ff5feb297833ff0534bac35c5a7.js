eba02f3cf29caf668e24d84885956a02a2f9339c
Ana <aparakaki17@gmail.com>
2018-10-23 14:41:00 -0700

    build

---
+++ build/precache-manifest.3ece7ff5feb297833ff0534bac35c5a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "efc8a0bed168c5c6af91",
+    "url": "/react-memory-game/static/js/runtime~main.efc8a0be.js"
+  },
+  {
+    "revision": "f951c6f89511347e44ab",
+    "url": "/react-memory-game/static/js/main.f951c6f8.chunk.js"
+  },
+  {
+    "revision": "d7a3d35d5aa43deaea55",
+    "url": "/react-memory-game/static/js/1.d7a3d35d.chunk.js"
+  },
+  {
+    "revision": "f951c6f89511347e44ab",
+    "url": "/react-memory-game/static/css/main.4f7828ee.chunk.css"
+  },
+  {
+    "revision": "6eb959d50d7366efbb5ea4f2bd27163c",
+    "url": "/react-memory-game/index.html"
+  }
+];
